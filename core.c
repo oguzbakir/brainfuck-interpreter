2cbcd1fa6e70d39a4ab674cfd5a83f46d77741f1
oguzbakir <b21627007@cs.hacettepe.edu.tr>
2018-05-05 19:23:48 +0300

    Added stack

--- core.c
+++ core.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Compile and execute signal defns
 #define COMPILATION_SUCCESS         0
 #define COMPILATION_FAILURE         1
 #define EXECUTE_SUCCESS             2 //TODO: Handle execute return codes
 #define EXECUTE_FAILURE             3
+
+//Size defns
+#define STACK_SIZE                  4096
+
+//Stack defns
+#define STACK_POP()                 (STACK[--SP])
+#define STACK_PUSH()                (STACK[SP++])
+#define STACK_IS_EMPTY()            (SP == 0)
+#define STACK_IS_FULL()             (SP == STACK_SIZE)
+
+
+static unsigned long STACK[STACK_SIZE];
+static unsigned int sp = 0;
 
 int compile_brainfuck(FILE* fp) {
     return COMPILATION_SUCCESS;
